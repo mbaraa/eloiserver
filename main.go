20110d13766b88f90975466599995e838ef1b9c0
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-01-28 11:59:58 +0000

    sync repos file if it doesn't exist

--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/mbaraa/eloiserver/apis"
 	"github.com/mbaraa/eloiserver/config"
 	"github.com/mbaraa/eloiserver/globals"
+	"github.com/mbaraa/eloiserver/utils/gposcrapper"
 	"github.com/mbaraa/eloiserver/utils/overlays"
 )
 
 func main() {
 	var err error
 	globals.Overlays, err = overlays.LoadOverlays()
+	if os.IsNotExist(err) {
+		fmt.Println("Downloading Overlays")
+		globals.Overlays = gposcrapper.GetOverlays()
+
+		f, err := os.Create("overlays.json")
+		if err != nil {
+			panic(err)
+		}
+
+		json.NewEncoder(f).Encode(globals.Overlays)
+	}
+
 	if err != nil {
 		panic(err)
 	}
