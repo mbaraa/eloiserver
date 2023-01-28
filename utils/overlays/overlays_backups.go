036e3889efc360f370447b9a1f3f85a22ee3d269
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-01-28 17:25:28 +0000

    changed backup file

--- utils/overlays/overlays_backups.go
+++ utils/overlays/overlays_backups.go
@@ -5,34 +5,37 @@ import (
 	"os"
 
 	"github.com/mbaraa/eloiserver/config"
+	"github.com/mbaraa/eloiserver/globals"
 	"github.com/mbaraa/eloiserver/models"
 )
 
-var overlaysFilePath = config.BackupDirectory() + "/overlays.json"
+var overlaysFilePath = config.BackupDirectory() + "/_overlays.json"
 
-func LoadOverlays() (map[string]*models.Overlay, error) {
+func LoadOverlays() error {
 	overlaysFile, err := os.Open(overlaysFilePath)
 	defer overlaysFile.Close()
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	overlays := make(map[string]*models.Overlay)
 
 	err = json.NewDecoder(overlaysFile).Decode(&overlays)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return overlays, nil
+	globals.Overlays = overlays
+	globals.Ebuilds = ExtractEbuilds(overlays)
+	return nil
 }
 
 func SaveOverlays(overlays map[string]*models.Overlay) error {
 	overlaysFile, err := os.Create(overlaysFilePath)
-	defer overlaysFile.Close()
 	if err != nil {
 		return err
 	}
+	defer overlaysFile.Close()
 
 	return json.NewEncoder(overlaysFile).Encode(overlays)
 }
